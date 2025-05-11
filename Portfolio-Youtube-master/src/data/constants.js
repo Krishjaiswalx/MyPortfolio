522fc18115da22335d48e1996c7cdb6187096701
Krish Jaiswal <68545764+Krishjaiswalx@users.noreply.github.com>
2025-05-11 19:14:38 +0530

    bio

--- Portfolio-Youtube-master/src/data/constants.js
+++ Portfolio-Youtube-master/src/data/constants.js
@@ -1,5 +1,5 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Krish Jaiswal",
   roles: [
     "Full Stack Developer",
     "Android Developer",
@@ -10,11 +10,11 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/rishavchanda",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+    "https://docs.google.com/document/d/1OTaj3Jk5N5QolVEkABo5c2ab8w5_KZJFgn1QUHLN0L4/edit?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/krishjaiswalx/",
+  twitter: "https://twitter.com/krishjaiswalx",
+  insta: "https://www.instagram.com/krishjaiswalx/",
+  facebook: "https://www.facebook.com/krishjaiswalx/",
 };
 
 export const skills = [
